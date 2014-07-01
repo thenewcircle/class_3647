f2c5362cf9c414e6f94494693939ffab7df82948
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2014-06-30 22:42:39 -0400

    Updated typos in UserResourceTest. Removed call to @GET.

--- src/test/java/chirp/service/resources/UserResourceTest.java
+++ src/test/java/chirp/service/resources/UserResourceTest.java
@@ -5,10 +5,7 @@ import static org.junit.Assert.assertNotNull;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Form;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import junit.framework.Assert;
 
 import org.junit.Test;
 
@@ -20,15 +17,17 @@ public class UserResourceTest extends JerseyResourceTest<UserResource> {
 	
 	@Test
 	public void createUserWithPOSTSuccess() {
+		
 		Form userForm = new Form();
 		userForm.param("realname", "John Doe");
 		userForm.param("username", "john.doe");
+		
 		Entity<Form> uploadData = Entity.form(userForm);
+
 		Response response = target("/users").request().post(uploadData);
 		assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
 		assertNotNull(users.getUser("john.doe"));
-		String realName = target("/users").path("john.doe").request().accept(MediaType.TEXT_PLAIN).get(String.class);
-		assertEquals("John Doe", realName);
+
 	}
 
 }
